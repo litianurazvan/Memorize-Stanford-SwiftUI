abbfe15d748ed75b356c23b73c115a7f2cc9a998
Razvan Litianu <razvan.litianu@prudential.com>
2020-10-23 14:06:35 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Razvan Litianu on 23/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
