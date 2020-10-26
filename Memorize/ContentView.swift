178ff223b956136229496b15d01c5b428efb4ade
Razvan Litianu <razvan.litianu@prudential.com>
2020-10-26 14:36:20 +0200

    Lecture 1 Create card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { _ in
+                CardView(isFaceUp: true)
+            }
+        }
+    }
+}
+struct CardView: View {
+    var isFaceUp: Bool
+
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).fill(Color.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 4)
+                    .foregroundColor(Color.orange)
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 4)
+                    .foregroundColor(Color.orange)
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
