10b4c705a0e878a3502c1382e82bdaa1c35517a0
Razvan Litianu <razvan.litianu@prudential.com>
2020-10-28 15:00:14 +0200

    Lecture 3 Reactive programming, UI adjustments

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,13 +14,13 @@ struct ContentView: View {
         HStack {
             ForEach(viewModel.cards) { card in
                 CardView(card: card)
+                    .aspectRatio(2/3, contentMode: .fit)
                     .onTapGesture {
                         viewModel.choose(card: card)
                     }
             }
         }
         .foregroundColor(Color.orange)
-        .font(.largeTitle)
         .padding()
     }
 }
@@ -28,17 +28,26 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
 
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 12).fill(Color.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(lineWidth: 4)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+        GeometryReader { geometryProxy in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .stroke(lineWidth: lineWidth)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                }
             }
+            .font(.system(size: min(geometryProxy.size.width, geometryProxy.size.height) * fontScaleFactor))
         }
     }
+
+    //MARK: - Drawing Constants
+
+    let cornerRadius: CGFloat = 12
+    let lineWidth: CGFloat = 4
+    let fontScaleFactor: CGFloat = 0.75
 }
 
 struct ContentView_Previews: PreviewProvider {
